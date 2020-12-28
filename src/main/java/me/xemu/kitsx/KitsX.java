947234130531a94968071e7a913c18dd1bf12f15
Code By Xemu <74688700+codebyxemu@users.noreply.github.com>
2020-12-28 04:09:43 +0100

    Plugin Init

---
+++ src/main/java/me/xemu/kitsx/KitsX.java
@@ -0,0 +1,19 @@
+// Code by Xemu (https://www.github.com/codebyxemu)
+package me.xemu.kitsx;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KitsX extends JavaPlugin
+{
+
+    @Override public void onEnable()
+    {
+
+    };
+
+    @Override public void onDisable()
+    {
+
+    };
+
+};
